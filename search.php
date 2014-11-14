da208079f7f4dc3763ff9491bc90cba562ab4e74
Devin Price <devin@wptheming.com>
2014-11-14 12:37:15 -0600

    Search and 404.

--- search.php
+++ search.php
@@ -7,32 +7,32 @@
 
 get_header(); ?>
 
-	<header class="page-header">
-		<h1 class="page-title"><?php printf( __( 'Search results for: %s', 'cascade' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-	</header><!-- .page-header -->
-
 	<section id="primary" class="content-area">
 
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+			<header class="page-header">
+				<h1 class="page-title"><?php printf( __( 'Search results for: %s', 'cascade' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			</header><!-- .page-header -->
 
-			<div id="posts-wrap">
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) : ?>
 
-				<?php get_template_part( 'content' ); ?>
+				<div id="posts-wrap">
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; ?>
-			</div>
+					<?php get_template_part( 'content' ); ?>
 
-			<?php cascade_paging_nav(); ?>
+				<?php endwhile; ?>
+				</div>
 
-		<?php else : ?>
+				<?php cascade_paging_nav(); ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+			<?php else : ?>
 
-		<?php endif; ?>
+				<?php get_template_part( 'content', 'none' ); ?>
+
+			<?php endif; ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
